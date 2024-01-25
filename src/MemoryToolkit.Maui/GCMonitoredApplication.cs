1b65be592a97bfc7c94a1bdbe3b1c86c32593f53
Adam Essenmacher <adam@scavos.com>
2024-01-25 02:58:40 -0500

    GCMonitoredApplication no longer responsible for monitoring for navigation events

--- src/MemoryToolkit.Maui/GCMonitoredApplication.cs
+++ src/MemoryToolkit.Maui/GCMonitoredApplication.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using Microsoft.Extensions.Logging;
 
 // ReSharper disable VirtualMemberNeverOverridden.Global
@@ -6,20 +5,11 @@ using Microsoft.Extensions.Logging;
 
 namespace MemoryToolkit.Maui;
 
-public class GCMonitoredApplication : Application
+public class GCMonitoredApplication(ILogger<GCMonitoredApplication> logger) : Application
 {
     public static readonly BindableProperty ShowMemToolkitAlertsProperty =
         BindableProperty.Create(nameof(ShowMemToolkitAlerts), typeof(bool), typeof(GCMonitoredApplication), true);
 
-    public GCMonitoredApplication(ILogger<GCMonitoredApplication> logger)
-    {
-        CollectionMonitor = GCCollectionMonitor.Instance;
-        Logger = logger;
-
-        PropertyChanged += HandleMainPageChanged;
-    }
-
-    protected IGCCollectionMonitor CollectionMonitor { get; set; }
     protected Page? CurrentPage { get; set; }
 
     public bool ShowMemToolkitAlerts
@@ -28,9 +18,9 @@ public class GCMonitoredApplication : Application
         set => SetValue(ShowMemToolkitAlertsProperty, value);
     }
 
-    public ILogger<GCMonitoredApplication> Logger { get; set; }
+    public ILogger<GCMonitoredApplication> Logger { get; set; } = logger;
 
-    protected virtual void OnLeaked(GCCollectionItem item)
+    public virtual void OnLeaked(GCCollectionItem item)
     {
         Logger.LogWarning("❗🧟❗{TargetName} is a zombie", item.Name);
 
@@ -38,64 +28,8 @@ public class GCMonitoredApplication : Application
             CurrentPage?.DisplayAlert("💦Leak Detected💦", $"❗🧟❗{item.Name} is a zombie!", "OK");
     }
 
-    protected virtual void OnCollected(GCCollectionItem item)
+    public virtual void OnCollected(GCCollectionItem item)
     {
         Logger.LogTrace("✅{TargetName} released", item.Name);
-    }
-
-    protected override Window CreateWindow(IActivationState? activationState)
-    {
-        Window window = base.CreateWindow(activationState);
-        CurrentPage = window.Page;
-        window.PropertyChanged += HandleWindowPageChanged;
-
-        return window;
-    }
-
-    public override void CloseWindow(Window window)
-    {
-        CurrentPage = null;
-        window.PropertyChanged -= HandleWindowPageChanged;
-        base.CloseWindow(window);
-    }
-
-    protected void HandleWindowPageChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (sender is not Window window)
-            return;
-
-        if (e.PropertyName != nameof(Window.Page))
-            return;
-
-        Page? lastPage = CurrentPage;
-        if (lastPage is NavigationPage lastNavPage)
-            lastNavPage.Popped -= NavPageOnPopped;
-
-        CurrentPage = window.Page;
-        if (CurrentPage is NavigationPage currentNavPage)
-            currentNavPage.Popped += NavPageOnPopped;
-
-        CollectionMonitor.ForceCollectionAsync(OnLeaked, OnCollected);
-    }
-
-    protected void HandleMainPageChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName != nameof(MainPage))
-            return;
-
-        Page? lastPage = CurrentPage;
-        if (lastPage is NavigationPage lastNavPage)
-            lastNavPage.Popped -= NavPageOnPopped;
-
-        CurrentPage = MainPage;
-        if (CurrentPage is NavigationPage currentNavPage)
-            currentNavPage.Popped += NavPageOnPopped;
-
-        CollectionMonitor.ForceCollectionAsync(OnLeaked, OnCollected);
-    }
-
-    protected void NavPageOnPopped(object? sender, NavigationEventArgs e)
-    {
-        CollectionMonitor.ForceCollectionAsync(OnLeaked, OnCollected);
     }
 }
